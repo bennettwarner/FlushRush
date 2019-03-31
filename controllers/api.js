fbefc95d7299fd1132185f2f4cb63ef402d1b48b
NBluder233 <41296084+NBluder233@users.noreply.github.com>
2019-03-31 03:13:33 -0400

    Update api.js

--- controllers/api.js
+++ controllers/api.js
@@ -9,7 +9,7 @@ exports.find = (req, res) => {
   //res.send('lat: ' + req.params.lat + ' long: ' + req.params.long);
 
   //pull everything within +-.30 degrees from lat and long 
-  const arr = bathroom.find({latitude: {$gte: req.params.lat - 0.3, $lte: req.params.lat + 0.3}, longitude: {$gte: req.params.long - 0.3, $lte: req.params.long + 0.3}});
-
-  res.send(arr);
+  const arr = bathroom.find({});
+  var bath = arr[0];
+  res.send('bathroom: ' + bath);
 };
