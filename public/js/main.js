0e754be7be4c640a22506e0cf575fc73ac7ff8fe
Bennett Warner <bennettdwarner@gmail.com>
2019-03-30 23:41:59 -0400

    add

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,19 @@
 /* eslint-env jquery, browser */
 $(document).ready(() => {
-
-
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(setPosition);
+    } else { 
+      console.log("Geolocation is not supported by this browser.");
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+  function setPosition(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    $('input[name=lat]').val(lat);
+    $('input[name=lon]').val(lon);
+  }
+  getLocation();
 
 });
