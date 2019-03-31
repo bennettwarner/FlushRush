8f88cbafc256b4aaff24ab794192b464627866d0
Bennett Warner <bennettdwarner@gmail.com>
2019-03-31 01:10:58 -0400

    add

--- public/js/main.js
+++ public/js/main.js
@@ -13,7 +13,31 @@ $(document).ready(() => {
     var lon = position.coords.longitude;
     $('input[name=lat]').val(lat);
     $('input[name=lon]').val(lon);
+    var request = new XMLHttpRequest()
+  
+    request.open('GET', 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+$('input[name=lat]').val()+','+$('input[name=lon]').val()+'&key=AIzaSyAumXpydW7Mo9r5QMTUqdWkPXcLNysTZyg', true)
+    request.onload = function() {
+      // Begin accessing JSON data here
+      var data = JSON.parse(this.response)
+      var address = ''
+      if (request.status >= 200 && request.status < 400) {
+        for (var i=0; i< data.results[0].address_components.length; i++ ){
+          address = address + ' ' + data.results[0].address_components[i].short_name;
+          if (i == data.results[0].address_components.length-1){
+            address = $.trim(address);
+            $('#address').val(address)
+            console.log(address);
+          }
+        }
+      } else {
+        console.log('error')
+      }
+    }
+    request.send()
   }
   getLocation();
+
+
+
 
 });
